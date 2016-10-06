62dda2e3a453267b2fbe641d713b277f1f11770c
Matt Wakeman <matt@datahobo.com>
2016-10-06 14:19:12 -0400

    more packages added

--- r-common-code/packages.R
+++ r-common-code/packages.R
@@ -1,2 +1,2 @@
 install.packages(c("plyr","stringr","ggplot2","reshape2","lattice","rpart",
-                   "sqldf","XML","car","dplyr"))
+                   "sqldf","XML","car","dplyr","lubridate","shiny"))
